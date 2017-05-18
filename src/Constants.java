bbec816e8a17c83a6dbd9f07933303257b9a0b54
mma <mma137421@gmail.com>
2017-05-18 19:53:17 +0430

    Implements some commands for server PI

---
+++ src/Constants.java
@@ -0,0 +1,14 @@
+/**
+ * Created by mma on 5/18/17.
+ */
+public class Constants {
+    public static String CODE_331 = "Username OK, password required";
+
+    public static String CODE_332 = "Need account for login";
+
+    public static String CODE_230 = "User logged in";
+
+    public static String CODE_530 = "Not logged in";
+
+    public static String CODE_200 = "Command OK";
+}
--- src/ServerPI.java
+++ src/ServerPI.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.net.Socket;
 
 /**
@@ -5,11 +6,69 @@ import java.net.Socket;
  */
 public class ServerPI implements Runnable {
     Socket connection;
-    public ServerPI(Socket connection){
+    boolean loggedIn = false;
+    String username = null;
+    String password = null;
+
+    public ServerPI(Socket connection) {
         this.connection = connection;
     }
+
     @Override
     public void run() {
+        BufferedReader inFromClient = null;
+        try {
+            inFromClient = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(
+                    connection.getOutputStream());
+            while (true) {
+                String clientSentece = inFromClient.readLine();
+                String command = clientSentece.split("\\s")[0];
+                String parameter = clientSentece.split("\\s")[1];
+                switch (command) {
+                    case "USER":
+                        username = parameter;
+                        if (username.equals("root"))
+                            outToClient.writeBytes("331 " + Constants.CODE_331);
+                        else
+                            outToClient.writeBytes("332 " + Constants.CODE_332);
+                        break;
+                    case "PASS":
+                        password = parameter;
+                        if ( username.equals("root") && password.equals("toor")){
+                            outToClient.writeBytes("230 " + Constants.CODE_230);
+                            loggedIn = true;
+                        }else{
+                            outToClient.writeBytes("332 " + Constants.CODE_332);
+                        }
+                        break;
+                    case "RMD":
+                        if ( loggedIn == true){
+                            File index = new File("/files");
+                            String[]entries = index.list();
+                            for(String s: entries){
+                                File currentFile = new File(index.getPath(),s);
+                                currentFile.delete();
+                            }
+                            outToClient.writeBytes("200 " + Constants.CODE_200);
+                        }else{
+                            outToClient.writeBytes("530 " + Constants.CODE_530);
+                        }
+                        break;
+                    case "DELE":
+                        break;
+                    case "RETR":
+                        break;
+                    case "LIST":
+                        break;
+                    case "QUIT":
+                        break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
