9b74faf0e3924dad3bb32236d2d802f25821b360
mma <mma137421@gmail.com>
2017-05-18 18:24:40 +0430

    Implements main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by mma on 5/18/17.
  */
 public class Main {
+    public static void  main(String args[]){
+        ServerSocket welcomeSocket;
+        try {
+            welcomeSocket = new ServerSocket(8000);
+            System.err.println("Welcoming socket in server was created!");
+            while (true) {
+                System.err.println("Waiting for connection on port "
+                        + welcomeSocket.getLocalPort());
+                Socket connectionSocket = welcomeSocket.accept();
+                System.err.println("New Client From "
+                        + connectionSocket.getInetAddress() + ":"
+                        + connectionSocket.getLocalPort() + " connected to server!");
+                new Thread(new ServerPI(connectionSocket)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- src/ServerPI.java
+++ src/ServerPI.java
@@ -1,7 +1,13 @@
+import java.net.Socket;
+
 /**
  * Created by mma on 5/18/17.
  */
 public class ServerPI implements Runnable {
+    Socket connection;
+    public ServerPI(Socket connection){
+        this.connection = connection;
+    }
     @Override
     public void run() {
 
