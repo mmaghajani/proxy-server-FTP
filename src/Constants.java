64708445a08cc6a78c7d17c417f70b613728d5f1
mma <mma137421@gmail.com>
2017-05-19 07:12:37 +0430

    USER and PASS and QUIT commands was test successfully

--- src/Constants.java
+++ src/Constants.java
@@ -2,17 +2,19 @@
  * Created by mma on 5/18/17.
  */
 public class Constants {
-    public static String PASSWORD_REQUIRED = "Username OK, password required";
+    public static String PASSWORD_REQUIRED = "Username OK, password required\n";
 
-    public static String NEED_ACCOUNT = "Need account for login";
+    public static String NEED_ACCOUNT = "Need account for login\n";
 
-    public static String LOGGED_IN = "User logged in";
+    public static String LOGGED_IN = "User logged in\n";
 
-    public static String NOT_LOGGED_IN = "Not logged in";
+    public static String NOT_LOGGED_IN = "Not logged in\n";
 
-    public static String OK = "Command OK";
+    public static String OK = "Command OK\n";
 
-    public static String FILE_NAME_NOT_ALLOWED = "File name not allowed";
+    public static String FILE_NAME_NOT_ALLOWED = "File name not allowed\n";
 
-    public static String CANT_OPEN_DATA_CONNECTION = "Can't open data connection";
+    public static String CANT_OPEN_DATA_CONNECTION = "Can't open data connection\n";
+
+    public static String COMMAND_NOT_IMPLEMENTED = "Command not implemented\n";
 }
