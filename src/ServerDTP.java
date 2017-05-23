b5a6b25c18d8074799450f8dada2ca5ff61e796c
mmaghajani <mma137421@gmail.com>
2017-05-23 20:06:55 +0430

    Updates ServerDTP

--- src/ServerDTP.java
+++ src/ServerDTP.java
@@ -19,6 +19,8 @@ public class ServerDTP {
         DataOutputStream outToClient = new DataOutputStream(
                 clientSocket.getOutputStream());
         outToClient.writeBytes(response + "\n");
+        outToClient.flush();
+        outToClient.close();
         clientSocket.close();
         System.err.println("Connection Closed!");
 
